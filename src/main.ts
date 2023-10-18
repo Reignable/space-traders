f1a998427280b44f51cbecaa4e6d87415d07373c
Reignable <joebarrett401@gmail.com>
2023-10-18 12:48:55 +0100

    add msw during serve

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
+import { isDevMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+async function prepareApp() {
+  if (isDevMode()) {
+    const { worker } = await import('./mocks/browser');
+    return worker.start();
+  }
+
+  return Promise.resolve();
+}
+
+prepareApp().then(() => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+});
