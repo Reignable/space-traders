fe1fc3f861b84db95d8395215b12f46cfdbe5d1f
Reignable <joebarrett401@gmail.com>
2023-10-18 11:35:25 +0100

    add msw

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker';
+import { PathParams, rest } from 'msw';
+import { RegisterRequest, RegisterResponse } from 'src/auth/types';
+
+export const handlers = [
+  rest.post<RegisterRequest, PathParams<never>, RegisterResponse>(
+    '/register',
+    async (req, res, ctx) => {
+      const { symbol, faction } = await req.json<RegisterRequest>();
+      return res(
+        ctx.json({
+          agent: {
+            accountId: faker.string.uuid(),
+            symbol: symbol.toUpperCase(),
+            startingFaction: faction,
+          },
+          token: faker.string.uuid(),
+        })
+      );
+    }
+  ),
+];
